5e60b9d5481f754843964f3da735d297046484ba
Stevenson <rstevenson@usgs.gov>
2018-09-17 12:12:10 -0600

    Updated Colors in NW for TN

--- src/scripts/sparrow-config.js
+++ src/scripts/sparrow-config.js
@@ -138,8 +138,8 @@ var nitrogenSourceDefinitions = {
     1. each color corresponds with the order of SourceDefinitions objects above  
     2. there the number of hex colors matches the number of nutrient sources
 **/
-var phosColors = ['#BF0000', '#FFCCFF', '#663100', '#FFEC99', '#A2EB85', '#0070C0' ];     
-var nitroColors = ['#BF0000', '#FFCCFF', '#663100', '#FFEC99', '#A2EB85', '#00a900', '#006800', '#0070C0' ];  
+var phosColors = ['#BF0000', '#FFCCFF', '#663100', '#FFEC99', '#A2EB85'];     
+var nitroColors = ['#BF0000', '#FFCCFF', '#663100', '#FFEC99', '#00a900','#A2EB85', '#006800', '#0070C0' ];  
 
 
 /***-----BEGIN PHOSPHORUS LAYER GROUPS --------***/
